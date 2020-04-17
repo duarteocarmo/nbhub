26a3541595ec9ab27e61acbbf908d78afa9187a0
Duarte OC <duarteocarmo@gmail.com>
2020-04-18 00:18:27 +0200

    95% testing

--- tests/test_nbhub.py
+++ tests/test_nbhub.py
@@ -1,21 +1,35 @@
-from click.testing import CliRunner
-from nbhub import nbhub
 import pathlib
+import pytest
+import nbhub
+from click.testing import CliRunner
+import click
 
 
-def test_nbhub():
+def test_nbhub_normal():
     runner = CliRunner()
     notebook = pathlib.Path.cwd() / "tests" / "data" / "test_notebook.ipynb"
-    result = runner.invoke(nbhub, [str(notebook)], input="\n".join(["y", "n"]))
+    result = runner.invoke(
+        nbhub.nbhub, [str(notebook)], input="\n".join(["y", "n"])
+    )
     assert result.exit_code == 0
     assert "Published" in result.output
+
+
+def test_nbhub_nothing():
+    runner = CliRunner()
+    notebook = pathlib.Path.cwd() / "tests" / "data" / "test_notebook.ipynb"
+    result = runner.invoke(nbhub.nbhub)
+    assert result.exit_code == 0
+    assert "No notebook provided, nothing to do" in result.output
 
 
 def test_nbhub_with_password():
     runner = CliRunner()
     notebook = pathlib.Path.cwd() / "tests" / "data" / "test_notebook.ipynb"
     print("hey")
-    result = runner.invoke(nbhub, [str(notebook)], input="\n".join(["y", "y"]))
+    result = runner.invoke(
+        nbhub.nbhub, [str(notebook)], input="\n".join(["y", "y"])
+    )
     assert result.exit_code == 0
     assert "not available yet" in result.output
 
@@ -23,6 +37,23 @@ def test_nbhub_with_password():
 def test_bad_file():
     runner = CliRunner()
     notebook = pathlib.Path.cwd() / "tests" / "data" / "bad.txt"
-    result = runner.invoke(nbhub, [str(notebook)], input="\n".join(["y", "n"]))
+    result = runner.invoke(
+        nbhub.nbhub, [str(notebook)], input="\n".join(["y", "n"])
+    )
     assert result.exit_code == 0
     assert "wrong" in result.output
+
+
+def test_check_notebook():
+    notebook = pathlib.Path.cwd() / "tests" / "data" / "test_notebook.ipynb"
+    assert nbhub.check_notebook(notebook) == None
+
+
+def test_check_status_good():
+    assert nbhub.status_ok("https://google.com") == True
+
+
+def test_check_status_bad():
+    with pytest.raises(click.exceptions.Exit) as f:
+        with pytest.raises(ConnectionError) as e:
+            assert nbhub.status_ok("https://google.aafeaf") == False
